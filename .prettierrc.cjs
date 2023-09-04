9cf4683bad9f8ac6ead151e9306ebc572ae998da
nemutas <inohiro.focru@gmail.com>
2023-09-04 19:33:56 +0900

    init

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+	plugins: ['prettier-plugin-astro'],
+	tabWidth: 2,
+	useTabs: false,
+	semi: false,
+	trailingComma: 'all',
+	singleQuote: true,
+	printWidth: 160,
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+[VSCode Astro v2.3.1](https://marketplace.visualstudio.com/items?itemName=astro-build.astro-vscode)
+
+`settings.json`
+
+```settings.json
+"prettier.documentSelectors": ["**/*.astro"],
+```
